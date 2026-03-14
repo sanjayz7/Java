b7c2762761835a5511a653d1d05a379ed2b05264
sanjuz7 <sanjayk91940@gmail.com>
2026-03-14 22:56:38 +0530

    Non Overlapping Problem

---
+++ DSA/Greedy/Non_Overlapping.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+            int ans=0;
+            
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int last=intervals[0][1];
+        for(int i = 1; i < intervals.length; i++){
+                   int start =intervals[i][0];
+                   
+                   if(start<last){
+                    ans++;
+                   }
+                   else{
+                    last=intervals[i][1];
+                   }
+
+        }
+        return ans; 
+    }
+}
